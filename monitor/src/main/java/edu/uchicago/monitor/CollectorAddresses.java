1906c917b07b97dcf65b0b5d5c19c50eeb55821f
ivukotic <ivukotic@uchicago.edu>
2012-10-08 11:23:16 -0500

    fix problem if one of the two monitoring streams not given.

--- monitor/src/main/java/edu/uchicago/monitor/CollectorAddresses.java
+++ monitor/src/main/java/edu/uchicago/monitor/CollectorAddresses.java
@@ -7,32 +7,42 @@ public class CollectorAddresses {
 
 	public ArrayList<Address> summary = new ArrayList<Address>();
 	public ArrayList<Address> detailed = new ArrayList<Address>();
-	
+
 	CollectorAddresses() {
 		Properties properties = System.getProperties();
-		
-		String pSummary = properties.getProperty("summary");
-		String[] tempSummary = pSummary.split(",");
-		for (int i = 0; i < tempSummary.length; i++) {
-			try {
-				summary.add(new Address(tempSummary[i]));
-			} catch (IllegalArgumentException e) {
-				System.err.println(e.getMessage());
-				System.exit(1);
-			} 
-		}
+		boolean on = false;
 
-		String pDetailed = properties.getProperty("detailed");
-		String[] tempDetailed = pDetailed.split(",");
-		for (int i = 0; i < tempDetailed.length; i++) {
-			try {
-				detailed.add(new Address(tempDetailed[i]));
-			} catch (IllegalArgumentException e) {
-				System.err.println(e.getMessage());
-				System.exit(1);
+		String pSummary = properties.getProperty("summary");
+		if (pSummary != null) {
+			String[] tempSummary = pSummary.split(",");
+			for (int i = 0; i < tempSummary.length; i++) {
+				try {
+					summary.add(new Address(tempSummary[i]));
+					on = true;
+				} catch (IllegalArgumentException e) {
+					System.err.println(e.getMessage());
+					System.exit(1);
+				}
 			}
 		}
 
+		String pDetailed = properties.getProperty("detailed");
+		if (pDetailed != null) {
+			String[] tempDetailed = pDetailed.split(",");
+			for (int i = 0; i < tempDetailed.length; i++) {
+				try {
+					detailed.add(new Address(tempDetailed[i]));
+					on = true;
+				} catch (IllegalArgumentException e) {
+					System.err.println(e.getMessage());
+					System.exit(1);
+				}
+			}
+		}
+		if (!on) {
+			System.err.println(" *** Err: No monitoring stream set-up. Set one up or remove monitoring plugin from configuration.");
+			System.exit(1);
+		}
 	}
 
 	@Override
