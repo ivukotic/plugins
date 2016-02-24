7a4eac904afd9f642db727b4a1cc5c4e23c2615b
Ilija Vukotic <ivukotic@cern.ch>
2016-02-24 16:21:59 -0600

    added interception of async responses.

--- redirect/src/main/java/edu/uchicago/redirector/RedirectPlugin.java
+++ redirect/src/main/java/edu/uchicago/redirector/RedirectPlugin.java
@@ -7,9 +7,11 @@ import io.netty.channel.ChannelPromise;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.dcache.xrootd.protocol.XrootdProtocol;
+import org.dcache.xrootd.protocol.messages.AsyncResponse;
 import org.dcache.xrootd.protocol.messages.ErrorResponse;
 import org.dcache.xrootd.protocol.messages.OpenRequest;
 import org.dcache.xrootd.protocol.messages.RedirectResponse;
+import org.dcache.xrootd.protocol.messages.XrootdResponse;
 
 public class RedirectPlugin extends ChannelOutboundHandlerAdapter
 {
@@ -28,17 +30,44 @@ public class RedirectPlugin extends ChannelOutboundHandlerAdapter
 	@Override
 	public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
 		
-        if (msg instanceof ErrorResponse) {
-            ErrorResponse<?> error = (ErrorResponse<?>) msg;
+		ErrorResponse<?> error=null;
+        
+		if (msg instanceof ErrorResponse){
+        	error = (ErrorResponse<?>) msg;
+        }
+        
+        if (msg instanceof AsyncResponse) {
+        	AsyncResponse<?> AR = (AsyncResponse<?>) msg;
+        	XrootdResponse<?> eresp=AR.getResponse();
+        	if (eresp instanceof ErrorResponse){
+        		error=(ErrorResponse<?>) eresp;
+        	}
+        }
+        
+        if (error!=null){
             String em=error.toString();
-            logger.debug("redirector intercepted error:"+em);
-            if (error.getRequest() instanceof OpenRequest && em.contains(String.valueOf(XrootdProtocol.kXR_NotFound)) ){
-            	// && error.getErrorNumber() == XrootdProtocol.kXR_NotFound) {
-                logger.debug("redirecting upstream");
-                msg = new RedirectResponse(error.getRequest(), host, port, "", "");
+            logger.debug("redirector intercepted error: "+em);
+            if (error.getRequest() instanceof OpenRequest){
+            	switch (error.getErrorNumber()){
+            		case XrootdProtocol.kXR_NotFound:
+            			logger.debug("file not found. redirecting upstream");
+                		msg = new RedirectResponse(error.getRequest(), host, port, "", "");
+            			break;
+            		case XrootdProtocol.kXR_IOError:
+                		logger.debug("IO error. redirecting upstream");
+                		msg = new RedirectResponse(error.getRequest(), host, port, "", "");
+                		break;
+            		case XrootdProtocol.kXR_FSError:
+                		logger.debug("FS error. redirecting upstream");
+                		msg = new RedirectResponse(error.getRequest(), host, port, "", "");
+//                		break;
+//            		case XrootdProtocol.kXR_NotAuthorized:
+//                		logger.debug("Not authorized. redirecting upstream");
+//                		msg = new RedirectResponse(error.getRequest(), host, port, "", "");
+            	} 
             }
         }
-		
+        
 		super.write(ctx, msg, promise);
 
 	}
